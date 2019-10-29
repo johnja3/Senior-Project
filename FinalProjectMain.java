187af649316a7107908c5b251bac44ed6e80d9fa
Jordan <jordanpowell1997@yahoo.com>
2019-10-29 13:43:33 -0400

    Update FinalProjectMain.java

--- FinalProjectMain.java
+++ FinalProjectMain.java
@@ -1,3 +1,8 @@
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+// DB imports
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -6,11 +11,22 @@ import java.sql.Statement;
 import java.sql.*;
 import com.microsoft.sqlserver.jdbc.*;
 
-public class FinalProjectMain {
-
-    // Connect to your database.
-    // Replace server name, username, and password with your credentials
-    public static void main(String[] args) {
+public class SeniorProject extends FinalProjectMain
+{   
+    
+    public static void student(){
+        System.out.println("Testing student");      
+    }
+    
+    public static void teacher(){
+        System.out.println("Testing teacher");
+    }
+    
+    public static void administrator(){
+        System.out.println("Testing admin");
+    }  
+    
+    public static void testDB(){    
         String connectionUrl =
                 "jdbc:sqlserver://bcs430-final-project.database.windows.net;"
                         + "database=OASIS ASSISTANT;"
@@ -19,7 +35,7 @@ public class FinalProjectMain {
                         + "encrypt=true;"
                         + "trustServerCertificate=true;"
                         + "loginTimeout=30;";
-           
+        
         Statement state = null;
         ResultSet resultSet = null;
 
@@ -42,98 +58,5 @@ public class FinalProjectMain {
         catch (Exception e) {
             e.printStackTrace();
         }
-    }
+    }  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import java.io.*;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class SeniorProject 
-{
-
-
-    public static void main(String[] args) 
-    {
-       
-        Scanner keyboard = new Scanner(System.in);
-        
-        
-        int selector;
-        System.out.println("Welcome to Oasis Assistant.");
-        System.out.println("1. Student");
-        System.out.println("2. Teacher");
-        System.out.println("3. Administrator");
-        System.out.println("4. Exit OASIS Assiatnt");
-        selector = keyboard.nextInt();
-        
-
-        
-        
-        
-        do{
-            switch (selector) {
-               
-                case 1:
-                    student();
-                    selector = 4;
-                    break;
-                case 2:
-                    teacher();
-                    selector = 4;
-                    break;
-                case 3:
-                    administrator();
-                    selector = 4;
-                    break;
-                case 4:
-                    System.out.println("Good luck with your semester!");
-                    break;
-                 default:
-                    System.out.println("You did not select one of the options. Please try again.");
-                    selector = keyboard.nextInt();
-                 
-            }
-        
-        }while (selector != 4);
-        
-    }
-    
-    
-    public static void student()
-    {
-        System.out.println("Testing student");
-        
-        
-        
-        
-    }
-    
-    public static void teacher()
-    {
-        
-        System.out.println("testing teacher");
-    }
-    
-    public static void administrator()
-    {
-        System.out.println("Testing admin");
-    }
-    
-    
-    
-    
-}
-*/
