89c1ba9906dd501f5aa1fb94f9fdf818375707a3
Jordan <jordanpowell1997@yahoo.com>
2019-10-31 14:05:43 -0400

    Update SeniorProject.java

--- SeniorProject.java
+++ SeniorProject.java
@@ -11,15 +11,48 @@ import java.sql.Statement;
 import java.sql.*;
 import com.microsoft.sqlserver.jdbc.*;
 
-public class FinalProjectMain {
-
-    public static void main(String[] args) {
+public class SeniorProject extends FinalProjectMain
+{   
+    
+    public static void student(){
+        System.out.println("Testing student prompt:");
+        
+        Scanner keyboard = new Scanner(System.in);
+        Scanner keyboard2 = new Scanner(System.in);
+        Scanner keyboard3 = new Scanner(System.in);
+        Scanner keyboard4 = new Scanner(System.in);
+        int amountClass;
+        System.out.println("How many classes are you going to take?");
+        amountClass = keyboard.nextInt();
+        System.out.println("Please enter the classes you want to take. To return please press 0:");
+        System.out.println("Example: (BCS102, MTH130, BUS101)");
+        String selector;
+        selector = keyboard2.nextLine();
+        
+        String[] toppings = new String[amountClass];
+        
+        toppings = selector.split(",");
+        
+        for(int i=0; i < toppings.length; i++){
+			System.out.print(toppings[i] + " " );
+        
+        }
+    }
+    
+    public static void teacher(){
+        System.out.println("Testing teacher");
+    }
+    
+    public static void administrator(){
+        System.out.println("Testing admin");
+    }
+    
+    public static void menu(){
         // Takes user input
           Scanner keyboard = new Scanner(System.in);
-        // Allows access to methods in senior project file
-          SeniorProject sp = new SeniorProject();
+          String selector;
           
-          int selector;
+          // Displays menu
           System.out.println("Welcome to Oasis Assistant.");
           System.out.println("___________________________");
           System.out.println("Please type an option:");
@@ -28,33 +61,64 @@ public class FinalProjectMain {
           System.out.println("3. Administrator");
           System.out.println("4. Exit OASIS Assiatnt");
           System.out.println("5. Test DB");
-          selector = keyboard.nextInt();
-          do{
-              switch (selector) {
-                  case 1:
-                      sp.student();
-                      
+          selector = keyboard.next();
+          
+          switch (selector) {
+                  case "1":
+                      student();
                       break;
-                  case 2:
-                      sp.teacher();
-                      selector = 4;
+                  case "2":
+                      teacher();
                       break;
-                  case 3:
-                      sp.administrator();
-                      selector = 4;
+                  case "3":
+                      administrator();
                       break;
-                  case 4:
+                  case "4":
                       System.out.println("Good luck with your semester!");
                       System.exit(0);
                       break;
-                  case 5:
+                  case "5":
                       System.out.println("Testing DB:");
-                      sp.testDB();
+                      testDB();
                       break;
                    default:
                       System.out.println("Input is incorrect, try again.");
-                      selector = keyboard.nextInt();
+                      System.out.println();
+                      menu();
               }
-            }while (selector != 4);
     }
+    
+    public static void testDB(){    
+        String connectionUrl =
+                "jdbc:sqlserver://bcs430-final-project.database.windows.net;"
+                        + "database=OASIS ASSISTANT;"
+                        + "user=farmingdale@bcs430-final-project;"
+                        + "password=bcs430w!;"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=true;"
+                        + "loginTimeout=30;";
+        
+        Statement state = null;
+        ResultSet resultSet = null;
+
+        try{
+            Connection con = DriverManager.getConnection(connectionUrl);
+                    String SQL = "SELECT * FROM dbo.BCS102 WHERE crse = 102;";
+                    state = con.createStatement();
+                    resultSet = state.executeQuery(SQL);
+                    
+                    while(resultSet.next()){
+                        System.out.println(resultSet.getString(1) +
+                                "    " + resultSet.getString(2) + "     " + resultSet.getString(3) + "     " + 
+                                resultSet.getString(4) + "     " + resultSet.getString(5) + "     " + 
+                                resultSet.getString(6) + "     " + resultSet.getString(7) + "     " + resultSet.getString(8) +
+                                "     " +resultSet.getString(9) + "     " +resultSet.getString(10) + "     " +resultSet.getString(11) + 
+                                "     "/* +resultSet.getString(12)*/);
+                    }
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }  
 }
